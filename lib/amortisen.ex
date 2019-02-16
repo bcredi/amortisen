df629c4a966e45c921d338da72bba3ca056f23fe
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2019-02-16 12:13:26 -0200

    First commit

---
+++ lib/amortisen.ex
@@ -0,0 +1,18 @@
+defmodule Amortisen do
+  @moduledoc """
+  Documentation for Amortisen.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Amortisen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/amortisen_test.exs
@@ -0,0 +1,8 @@
+defmodule AmortisenTest do
+  use ExUnit.Case
+  doctest Amortisen
+
+  test "greets the world" do
+    assert Amortisen.hello() == :world
+  end
+end
