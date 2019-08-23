7b772d23ec7a19bd0d8dc316cdafc935db960194
Daniel Costa <eng.daniels.costa@gmail.com>
2019-08-23 11:34:01 -0300

    add life_insurance_fee and realty_insurance_fee to insurance calculation

--- lib/amortisen/monthly_extra_payments.ex
+++ lib/amortisen/monthly_extra_payments.ex
@@ -4,48 +4,29 @@ defmodule Amortisen.MonthlyExtraPayments do
   like insurances and administration fees.
   """
 
-  @life_insurance_fee 0.027
-
   @doc """
   Returns the product of `outstanding_balance` by the insurance fee.
 
   ## Examples
 
-      iex> life_insurance_amount(%Money{amount: 1_000_000})
+      iex> life_insurance_amount(%Money{amount: 1_000_000}, 0.027)
       %Money{amount: 48_000}
   """
-  @spec life_insurance_amount(Money.t()) :: Money.t()
-  def life_insurance_amount(%Money{} = outstanding_balance) do
-    Money.multiply(outstanding_balance, @life_insurance_fee / 100)
+  @spec life_insurance_amount(Money.t(), float()) :: Money.t()
+  def life_insurance_amount(%Money{} = outstanding_balance, life_insurance_fee) do
+    Money.multiply(outstanding_balance, life_insurance_fee / 100)
   end
-
-  @realty_insurance_fee 0.016
 
   @doc """
   Returns the product of `realty_value` by the insurance fee.
 
   ## Examples
 
-      iex> realty_insurance_amount(%Money{amount: 1_000_000})
+      iex> realty_insurance_amount(%Money{amount: 1_000_000}, 0.016)
       %Money{amount: 25_000}
   """
-  @spec realty_insurance_amount(Money.t()) :: Money.t()
-  def realty_insurance_amount(%Money{} = realty_value) do
-    Money.multiply(realty_value, @realty_insurance_fee / 100)
-  end
-
-  @monthly_administration_fee 0.0
-
-  @doc """
-  Returns the sum of `outstanding_balance` with administration fees.
-
-  ## Examples
-
-      iex> monthly_administration_amount()
-      %Money{amount: 1_000_000}
-  """
-  @spec monthly_administration_amount() :: Money.t()
-  def monthly_administration_amount do
-    Money.parse!(@monthly_administration_fee)
+  @spec realty_insurance_amount(Money.t(), float()) :: Money.t()
+  def realty_insurance_amount(%Money{} = realty_value, realty_insurance_fee) do
+    Money.multiply(realty_value, realty_insurance_fee / 100)
   end
 end
