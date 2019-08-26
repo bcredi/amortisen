15b2aebc6831dd1813f961656192f3420e86f449
Daniel Costa <eng.daniels.costa@gmail.com>
2019-08-26 11:10:00 -0300

    change lib version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Amortisen.MixProject do
   def project do
     [
       app: :amortisen,
-      version: "0.3.0",
+      version: "0.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
